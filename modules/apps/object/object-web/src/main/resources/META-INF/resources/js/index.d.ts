93d72d5a4df56a72136b53188a3a7fbec34d555d
MariMalta <maltamariana123@gmail.com>
2022-04-25 18:08:23 -0300

    LPS-151713/LPS-151714 Create interfaces

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/index.d.ts
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/index.d.ts
@@ -12,8 +12,9 @@
  * details.
  */
 
-type ObjectFieldBusinessType = 'Attachment' | 'LongText' | 'Picklist' | 'Text';
+import {MouseEventHandler} from 'react';
 
+type ObjectFieldBusinessType = 'Attachment' | 'LongText' | 'Picklist' | 'Text';
 interface ObjectFieldType {
 	businessType: ObjectFieldBusinessType;
 	dbType: string;
@@ -52,7 +53,7 @@ type ObjectFieldSettingName =
 interface ObjectValidation {
 	active: boolean;
 	description?: string;
-	engine: ObjectValidationType;
+	engine: string;
 	errorLabel: LocalizedValue<string>;
 	id: number;
 	name: any;
@@ -65,8 +66,9 @@ interface ObjectValidationRuleElement {
 }
 
 interface ObjectValidationRuleElementItem {
-	content: string;
+	content?: string;
 	label: string;
+	onClick?: MouseEventHandler;
 	tooltip: string;
 }
 
